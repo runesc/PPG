a66376e096d3a46f51cf27d337b431d777d7de65
Luis Alfredo Reyes <fredogodofredodospuntosv@gmail.com>
2021-10-28 10:15:41 -0500

    v1.0.2
    -Modified Notice
    -Fixed component generator

--- ppg/builtin_commands/components.py
+++ ppg/builtin_commands/components.py
@@ -2,9 +2,9 @@ component_template = f"""
 from ppg_runtime.application_context.$Binding import PPGLifeCycle
 from $Binding.QtWidgets import $Widget
 
-class $Name(PPGLifeCycle, $Widget):
+class $Name($Widget, PPGLifeCycle):
 	def __init__(self, parent=None, **kwargs):
-		super().__init__(parent=parent, objectName="$Name")
+		super().__init__(parent=parent, objectName="$Name", **kwargs)
 		self.parent = parent
 		self.store = parent.store
 

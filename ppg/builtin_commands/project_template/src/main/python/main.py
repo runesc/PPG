3ff22c2b08239f66640fb662bd53b41cdcfd6662
runesc <luisalfredoreyes98@gmail.com>
2024-11-27 02:22:43 -0600

    fix(builtin_commands app name fixed): myapp now supports custom name

--- ppg/builtin_commands/project_template/src/main/python/main.py
+++ ppg/builtin_commands/project_template/src/main/python/main.py
@@ -4,7 +4,7 @@ from ppg_runtime.application_context import PPGStore, PPGLifeCycle, init_lifecyc
 from ${python_bindings}.QtWidgets import QMainWindow, QLabel
 
 @init_lifecycle
-class MyApp(QMainWindow, PPGLifeCycle, PPGStore):
+class ${app_name}(QMainWindow, PPGLifeCycle, PPGStore):
     def component_will_mount(self):
         self.subscribe_to_store(self)
 
@@ -14,10 +14,13 @@ class MyApp(QMainWindow, PPGLifeCycle, PPGStore):
     def responsive_UI(self):
         self.setMinimumSize(640, 480)
 
+    def update_store(self, store):
+        pass
+
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()
-    window = MyApp()
+    window = ${app_name}()
     window.show()
     # This fixes the issue with PySide2 that the exec function is not found
     exec_func = getattr(appctxt.app, 'exec', appctxt.app.exec_)
